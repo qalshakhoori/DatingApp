d6fa2444a8ebeff13f274dc5762c1a8cd999ec8e
Qassim Mohammed Alshakhoori <qalshakhoori@moic.gov.bh>
2020-03-12 08:48:33 +0300

    lecture 80

---
+++ DatingApp.API/Dtos/PhotosForDetailedDto.cs
@@ -0,0 +1,14 @@
+using System;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Dtos
+{
+  public class PhotosForDetailedDto
+  {
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public string Description { get; set; }
+    public DateTime DateAdded { get; set; }
+    public bool IsMain { get; set; }
+  }
+}
