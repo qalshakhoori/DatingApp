4e16a3facebae4ea1bfa49c686874a4219d9d727
Qassim Mohammed Alshakhoori <qalshakhoori@moic.gov.bh>
2020-03-15 09:01:25 +0300

    lecture 85

--- DatingApp-SPA/src/app/members-list/members-list.component.ts
+++ DatingApp-SPA/src/app/members-list/members-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+import { AlertifyjsService } from '../_services/alertifyjs.service';
 
 @Component({
   selector: 'app-members-list',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./members-list.component.css']
 })
 export class MembersListComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private alertify: AlertifyjsService
+  ) {}
 
   ngOnInit() {
+    this.loadUsers();
   }
 
+  loadUsers() {
+    this.userService.getUsers().subscribe(
+      (users: User[]) => {
+        this.users = users;
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
+  }
 }
