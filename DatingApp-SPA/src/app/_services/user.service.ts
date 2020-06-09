e21b308b0c9760935e0c38f7c6bbb2319c3aa07f
Qassim Mohammed Alshakhoori <qalshakhoori@moic.gov.bh>
2020-06-09 09:34:28 +0300

    lecture 106

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   baseUrl = environment.apiUrl;
@@ -18,5 +18,9 @@ export class UserService {
 
   getUser(id: number): Observable<User> {
     return this.http.get<User>(this.baseUrl + 'users/' + id);
+  }
+
+  updateUser(id: number, user: User) {
+    return this.http.put(this.baseUrl + 'users/' + id, user);
   }
 }
