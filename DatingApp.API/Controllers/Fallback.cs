f2825c6cb7fdd782c8914e6d8d31c9c654436ee3
Qassim Mohammed Alshakhoori <qalshakhoori@moic.gov.bh>
2020-08-18 10:13:38 +0300

    production builds

---
+++ DatingApp.API/Controllers/Fallback.cs
@@ -0,0 +1,14 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+namespace DatingApp.API.Controllers
+{
+  [AllowAnonymous]
+  public class Fallback : Controller
+  {
+    public IActionResult Index()
+    {
+      return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+    }
+  }
+}
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -122,13 +122,18 @@ namespace DatingApp.API
 
       app.UseRouting();
 
-      app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
       app.UseAuthentication();
       app.UseAuthorization();
+
+      app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
 
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
+        endpoints.MapFallbackToController("Index", "Fallback");
       });
     }
   }
